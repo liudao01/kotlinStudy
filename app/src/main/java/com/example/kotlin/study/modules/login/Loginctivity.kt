43bc2b10b9e4ff9a94b41d5799e0c177960c8330
Liuml <951442843@qq.com>
2021-05-03 13:12:15 +0800

    login页面

---
+++ app/src/main/java/com/example/kotlin/study/modules/login/Loginctivity.kt
@@ -0,0 +1,15 @@
+package com.example.kotlin.study.modules.login
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.kotlin.study.R
+
+/**
+ * 登录界面
+ */
+class Loginctivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/example/kotlin/study/modules/test.java
@@ -0,0 +1,9 @@
+package com.example.kotlin.study.modules;
+
+/**
+ * @author liuml
+ * @explain
+ * @time 2021/5/3 13:10
+ */
+public class test {
+}
